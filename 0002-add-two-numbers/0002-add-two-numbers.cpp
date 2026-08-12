18ff1832b8369062ca5cbfc7bf9cec75eb0c8df1
Saksham_Khatri <sakshamkhatri2006@gmail.com>
2026-08-12 23:32:42 +0530

    Time: 0 ms (100%), Space: 77.1 MB (46.75%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
+        ListNode* dummy= new ListNode(0);
+        ListNode* temp= dummy;
+        int len1=0;
+        int len2=0;
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry!=NULL){
+            int sum = 0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            temp->next= new ListNode(sum%10);
+            temp=temp->next;
+        }
+        return dummy->next;
+
+        
+    }
+};
