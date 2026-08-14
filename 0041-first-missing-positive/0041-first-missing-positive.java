6b0a18cfde67abb1f07dbffcc94023e72071df44
Saksham_Khatri <sakshamkhatri2006@gmail.com>
2026-08-14 13:12:21 +0530

    Time: 1 ms (99.96%), Space: 71.1 MB (86.07%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -4,10 +4,12 @@ class Solution {
             arr[i]=arr[index];
             arr[index]=temp;
         }
+
+    // using cyclic sort
     public int firstMissingPositive(int[] arr) {
         int i=0;
         int n = arr.length;
-        while(i<n){
+        while(i<n){            
             if(arr[i]<=0 || arr[i]>n || arr[i]==i+1 || arr[i]==arr[arr[i]-1]) i++;
             else swap(arr,i,arr[i]-1);
         }
@@ -15,5 +17,10 @@ class Solution {
             if(arr[j]!=j+1) return j+1;
         }
         return n+1;
+        // arr[i]<=0  //agr negative or 0 hai  (Ignore)
+        // arr[i]>n  // element bda hai length se (Ignore)
+        // arr[i]==i+1. // agr shi position pe hai ignore 
+        // arr[i]==arr[arr[i]-1] duplicate hai chordo
+
     }
 }
